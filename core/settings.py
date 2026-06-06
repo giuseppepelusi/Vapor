13ef140dad74bcb2999b6d8d0d0904612c627c58
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2026-06-06 20:11:47 +0200

    Added templates dir

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "store" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
