2cfa7ead8987548db483e96d2ff0e31cb22c5fe1
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2026-06-10 11:50:52 +0200

    Personalized Admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import Game, Genre, Purchase, Review, UserProfile, Wishlist
+
+admin.site.unregister(Group)
 
 admin.site.register(UserProfile)
 admin.site.register(Genre)
@@ -8,3 +11,7 @@ admin.site.register(Game)
 admin.site.register(Purchase)
 admin.site.register(Wishlist)
 admin.site.register(Review)
+
+admin.site.site_header = admin.site.site_title = admin.site.index_title = (
+    "Vapor Admin Panel"
+)
